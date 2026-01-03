fcf69ccd7ca6a92491809b2a0dd1b0a64a48397a
keerthivasan91 <keerthivasan1617@gmail.com>
2026-01-04 01:25:36 +0530

    Add main application functionality with routes for home, about, registration, venue, submit paper, call for papers, UTCA2023, contact, and committee

---
+++ app.py
@@ -0,0 +1,86 @@
+from flask import Flask, render_template, request, flash
+import os
+from flask_mail import Mail, Message # type: ignore
+
+registration_fees = [
+    {
+        "category": "Research Scholar / UG / PG (Only)",
+        "national": "₹10,000",
+        "international": "$200"
+    },
+    {
+        "category": "Faculties / Scientists",
+        "national": "₹12,000",
+        "international": "$300"
+    },
+    {
+        "category": "Industry Person",
+        "national": "₹14,000",
+        "international": "$400"
+    }
+]
+
+app = Flask(__name__)
+app.secret_key = os.getenv("SECRET_KEY", "fallback-secret-key")
+
+@app.route('/')
+def home():
+    return render_template('index.html',active_page='home')
+
+@app.route('/about')
+def about():
+    return render_template('about.html',active_page='about')
+
+@app.route('/registration')
+def registration():
+    return render_template('registration.html',active_page='registration', fees=registration_fees)
+
+@app.route('/venue')
+def venue():
+    return render_template('venue.html',active_page='venue')
+
+@app.route('/submitpaper')
+def submitpaper():
+    return render_template('submitpaper.html',active_page='submitpaper')
+
+@app.route('/callforpapers')
+def callforpapers():
+    return render_template('callforpapers.html',active_page='callforpapers')
+
+@app.route('/UTCA2023')
+def UTCA2023():
+    return render_template('UTCA2023.html',active_page='UTCA2023')
+
+'''@app.route('/contact', methods=['GET', 'POST'])
+def contact():
+    if request.method == 'POST':
+        name = request.form.get('name') or "Anonymous"
+        email = request.form.get('email') or "Not provided"
+        message_content = request.form.get('message') or "No message"
+
+        recipient = os.getenv("MAIL_USERNAME")
+        if not recipient:
+            flash("Mail username not configured!", "error")
+            return render_template('contact.html', success=False, active_page='contact')
+
+        msg = Message(
+            subject=f"New Contact Form Message from {name}",
+            recipients=[recipient]
+        )
+        msg.body = f"Name: {name}\nEmail: {email}\n\nMessage:\n{message_content}"
+
+
+    return render_template('contact.html', success=None, active_page='contact')'''
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html", active_page="contact")
+
+
+@app.route("/committee")
+def committee():
+    return render_template("committee.html", active_page="committee")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
